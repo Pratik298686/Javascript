a607ebddccff160647d5a18acd37028a1269a99e
Pratik <pratikpadman125@gmail.com>
2021-09-25 14:04:14 +0530

    Add files via upload

---
+++ Program53.js
@@ -0,0 +1,20 @@
+console.log("this is program 53");
+
+function greet(name,GreetText){
+    console.log(GreetText+""+name);
+    console.log(name+"is a good boy"); 
+}
+let name="Pratik";
+let name1="Shubham";
+let name2="Nilesh";
+let name3="Suresh";
+let GreetText = "Good Morning";
+greet(name,GreetText);
+greet(name1,GreetText);
+greet(name2,GreetText);
+greet(name3,GreetText);
+
+/*console.log(name+"is a good boy");
+console.log(name1+"is a good boy");
+console.log(name2+"is a good boy");
+console.log(name3+"is a good boy");*/
